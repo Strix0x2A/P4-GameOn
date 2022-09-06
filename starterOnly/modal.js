8ab551e25826143a036d0faf41c5ddda37b9caef
Clémence Bergon <clem.bergon@gmail.com>
2022-09-06 21:06:27 +0200

    added closeModal() feature

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,19 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
